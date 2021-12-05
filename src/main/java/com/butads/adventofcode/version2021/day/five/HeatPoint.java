130040f01bb70524e6807adcf8da668751fb689f
Mark Bertels <mbertels-ext@degiro.nl>
2021-12-05 15:37:25 +0100

    Add day 5

---
+++ src/main/java/com/butads/adventofcode/version2021/day/five/HeatPoint.java
@@ -0,0 +1,16 @@
+package com.butads.adventofcode.version2021.day.five;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@ToString
+@Getter
+@EqualsAndHashCode
+public class HeatPoint {
+
+    private final int x;
+    private final int y;
+}
---
+++ src/main/java/com/butads/adventofcode/version2021/day/five/SecureHeatLocationDiagonal.java
@@ -0,0 +1,29 @@
+package com.butads.adventofcode.version2021.day.five;
+
+import static com.butads.adventofcode.BufferUtils.getInputAsStringList;
+
+import com.butads.adventofcode.version2021.Answerable;
+import java.io.IOException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SecureHeatLocationDiagonal implements Answerable {
+
+    private static final String LOCATION = "/2021/day5/input.txt";
+
+    @Override
+    public String getAnswer() throws IOException {
+        HeatGrid grid = new HeatGrid(HeatLine.createHeatlines(getInputAsStringList(LOCATION)));
+        return "" + grid.getPointsOverTwo(true).size();
+    }
+
+    @Override
+    public int getDay() {
+        return 5;
+    }
+
+    @Override
+    public int getSection() {
+        return 2;
+    }
+}
